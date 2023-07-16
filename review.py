556c82c14818134585ad32116ccf2ee3c2140ccd
RaziSa24-MEET <139680922+RaziSa24-MEET@users.noreply.github.com>
2023-07-16 18:59:25 +0300

    Create review.py

---
+++ review.py
@@ -0,0 +1,10 @@
+def youtube_video():
+	youtube_video={"title":"funkey town"}
+	youtube_video={"description":"fish"}
+	youtube_video={"likes":"0"}
+	youtube_video={"dislikes":"0"}
+	youtube_video={"username":input"comments"}
+deflikes():
+	likes=input youtube_video
+
+
