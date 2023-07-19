fe1c43c8a32eba8ca8cd99dac58ca19327d5688a
RaziSa24-MEET <139680922+RaziSa24-MEET@users.noreply.github.com>
2023-07-19 20:33:48 +0300

    Update review.py

--- review.py
+++ review.py
@@ -1,10 +1,28 @@
-def youtube_video():
-	youtube_video={"title":"funkey town"}
-	youtube_video={"description":"fish"}
-	youtube_video={"likes":"0"}
-	youtube_video={"dislikes":"0"}
-	youtube_video={"username":input"comments"}
-deflikes():
-	likes=input youtube_video
+def create_youtube_video(title,discription):
+	vid={"title" : title,
+	"discription" :discription,
+	"likes" : 0,
+	"dislikes" :0,
+	"comments":{}}
+	return vid 
+
+def likes(youtubevideo):
+	if "likes" in youtubevideo:
+		youtubevideo["likes"]+=1
 
 
+def dislikes(youtubevideo):
+	if "dislikes" in youtubevideo:
+		youtubevideo["dislikes"]+=1
+
+def add_comment(youtubevideo ,username ,comment_text):
+	youtubevideo["comments"][username]=comment_text
+
+vid1 = create_youtube_video('ali' ,"best ta")
+print(vid1)
+
+for anything in range(495):
+	likes(vid1)
+dislikes(vid1)
+add_comment(vid1 ,"meet" ,"y2")
+print(vid1)
